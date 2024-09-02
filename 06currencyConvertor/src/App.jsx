67bd668693dbf0cca21f0b5707fca7190b01bc88
AnupamDandriyal <112267752+AnupamDandriyal@users.noreply.github.com>
2024-09-02 21:30:18 +0530

    started the currency convertor

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,17 @@
+/* import { useState } from 'react' */
+import bgImage from '/img/pexels-jitu-mondal-3052645-4627668.jpg'
+
+import './App.css'
+
+function App() {
+  /* const [count, setCount] = useState(0)
+ */
+  return (
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+    style={{backgroundImage:`url(${bgImage})`}}>
+      
+   </div>
+  )
+}
+
+export default App
