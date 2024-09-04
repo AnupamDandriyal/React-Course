c5f85e5ead6d1afe8c5ea68d3a922fa46438affa
AnupamDandriyal <112267752+AnupamDandriyal@users.noreply.github.com>
2024-09-04 14:23:28 +0530

    done with mini Context

--- 08miniContext/src/Components/Login.jsx
+++ 08miniContext/src/Components/Login.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useContext} from 'react'
+import UserContext from '../Context/UserContext';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState(''); 
+  const {setUser} = useContext(UserContext)
   const handleSubmit = (e) => {
-    
+    e.preventDefault();
+    setUser({username,password})
   }
   return (
     <div>
-      <h2>Login</h2>
+      <h1>Login</h1>
       <input type="text"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
--- 08miniContext/src/Components/Profile.jsx
+++ 08miniContext/src/Components/Profile.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserContext from '../Context/UserContext'
 
 function Profile() {
+  const {user} = useContext(UserContext);
+  if(!user) return <h2>Not logged in!</h2>
   return (
-    <div>Profile</div>
+    <div>
+      <h2>Profile : {user.username}</h2>
+    </div>
   )
 }
 
