a03eea0271d2c0c9dac58f53e70b2fe6dc0591dd
AnupamDandriyal <112267752+AnupamDandriyal@users.noreply.github.com>
2024-09-21 14:13:44 +0530

    completed

--- BMICalc/src/App.jsx
+++ BMICalc/src/App.jsx
@@ -1,34 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState(''); 
+  const [bmi, setBMI] = useState();
+  const [result, setResult] = useState(null);
+
+  const reload = () => {
+    window.location.reload()
+  }
+
+  let calcBMI = (e) => {
+    e.preventDefault();
+    if (weight == 0 || height == 0 || weight == '' || height == '') {
+      alert('Please enter the valid data!')
+    }
+    else {
+      let bmi = ((weight) / (height * height)) * 703
+      setBMI(`Your BMI is : ${bmi.toFixed(2)}`);
+
+      if (bmi < 25) {
+        setResult('You are: underweight');
+      }
+      else if (bmi >= 25 && bmi < 30) {
+        setResult('You are: healthy')
+      }
+      else {
+        setResult('You are: over-weight')
+      }
+    }
+    
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <h1>BMI Calculator</h1>
+      <form onSubmit={calcBMI}>
+        <div>
+          <label htmlFor="weight">Weight (lbs): </label>
+          <input type="text" name='weight' placeholder='Weight in pounds' value={weight}
+            onChange={(e)=>setWeight(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="height">Height (in): </label>
+          <input type="text" name='height' placeholder='Height in inches' value={height}
+             onChange={(e)=>setHeight(e.target.value)}
+          />
+        </div>
+        <div className='btns'>
+          <button className='submit' type='submit'>Submit</button>
+          <button type='submit' onClick={reload}>ReLoad</button>
+        </div>
+        
+        <div>
+          <h3>{bmi} </h3>
+          <p>{result}</p>
+        </div>
+      </form>
+    </div>
   )
 }
 
