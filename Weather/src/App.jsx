fdc8e1b7d97cd471b3a63093cc2e92e3aa0e1cc3
AnupamDandriyal <112267752+AnupamDandriyal@users.noreply.github.com>
2024-09-22 21:22:53 +0530

    completed the weather app

---
+++ Weather/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="container_date">May 03, 2024</div>
+        <div className="container_city">Delhi</div>
+        <img src="" alt="climate" />
+        <div className='degrees'>
+          <div className="container_degree">20 </div>
+          <div className="degree_icon">&deg;</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
