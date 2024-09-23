cf0e0bfa425474dcf8e455fc223c920354ba0906
AnupamDandriyal <112267752+AnupamDandriyal@users.noreply.github.com>
2024-09-23 14:30:45 +0530

    started the siginpg and added the logo of twitter in it

---
+++ signinpg/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+import { BsTwitterX } from "react-icons/bs";
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="logo-box">
+        <div className="logo">
+          <BsTwitterX />
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
