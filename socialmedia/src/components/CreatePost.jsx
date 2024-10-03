dc22d75ce18082c0c51888dd37cabe5f68c6192b
AnupamDandriyal <112267752+AnupamDandriyal@users.noreply.github.com>
2024-10-03 13:57:20 +0530

    UI of create post is complete

--- socialmedia/src/components/CreatePost.jsx
+++ socialmedia/src/components/CreatePost.jsx
@@ -3,21 +3,33 @@
 const CreatePost = () => {
   return (
     <>
-      <form>
+      <form className="create-post">
+
+
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We will never share your email with anyone else.</div>
+          <label htmlFor="userID" className="form-label">User ID</label>
+          <input type="text" className="form-control" id="userID" aria-describedby="userID" placeholder="user-0"/>
         </div>
+
+
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
+          <label htmlFor="title" className="form-label">Post Title</label>
+          <input type="text" className="form-control" id="title" aria-describedby="title" placeholder="Add title to your post..."/>
         </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+      
+
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">About Your Post</label>
+          <textarea style={{ overflowY: 'auto', resize: 'vertical' }}  rows={2} type="text" className="form-control" id="description" aria-describedby="description" placeholder="Add caption of your post..."/>
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+
+
+        <div className="mb-3">
+          <label htmlFor="tags" className="form-label">HashTags</label>
+          <input type="text" className="form-control" id="tags" aria-describedby="tags" placeholder="#newpost (add space after each tag)"/>
+        </div>
+
+        <button type="submit" className="btn btn-primary createBtn">Create +</button>
       </form>
     </>
   )
