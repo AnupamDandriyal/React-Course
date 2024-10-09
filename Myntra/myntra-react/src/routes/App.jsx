b3250982229925b60edd15d780f88cd5622777f7
AnupamDandriyal <112267752+AnupamDandriyal@users.noreply.github.com>
2024-10-09 14:52:21 +0530

    completed the home page and now fetching the data from the server that need to be displayed

--- Myntra/myntra-react/src/routes/App.jsx
+++ Myntra/myntra-react/src/routes/App.jsx
@@ -2,14 +2,17 @@ import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import FetchItems from "../components/FetchItems.jsx";
+import { useSelector } from "react-redux";
+import LoadingSpinner from "../components/LoadingSpinner.jsx";
 
 function App() {
-
-
+  const fetchStatus = useSelector((store)=>store.fetchStatus)
   return (
     <>
       <Header />
-      <Outlet />
+      <FetchItems />
+      {fetchStatus.currentlyFetching ? <LoadingSpinner/>:<Outlet />}
       <Footer />
     </>
   );
