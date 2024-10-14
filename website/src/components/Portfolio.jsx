1679d2d86e2b260b8f5cd07e2898efb8efef4969
AnupamDandriyal <112267752+AnupamDandriyal@users.noreply.github.com>
2024-10-14 21:29:04 +0530

    completed the portfolio

--- website/src/components/Portfolio.jsx
+++ website/src/components/Portfolio.jsx
@@ -1,14 +1,35 @@
-
+import {useState} from 'react'
 import { portfolio } from "./data/dummydata"
 import { Visibility } from "@mui/icons-material"
 
+const allCategory = ['all', ...new Set(portfolio.map((item) => item.category))];
 
 const Portfolio = () => {
+  const [list, setList] = useState(portfolio);
+  const [category, setCategory] = useState(allCategory);
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setList(portfolio);
+    }
+    else {
+      const newItems = portfolio.filter((item) => item.category === category);
+      setList(newItems)
+    }
+   
+
+  }
   return (
     <div className="portfolio">
       <div className="container">
+        <h1>Portfolio</h1>
+        <div className="filterBtns">
+          {category.map((category, i) => (
+            <button key={i} className='primaryBtn'
+            onClick={()=>filterItems(category)}>{category}</button>
+          ))}
+        </div>
         <div className="grid3">
-          {portfolio.map((item, i) => (
+          {list.map((item, i) => (
             <div key={i} className="box">
               <img src={item.cover} alt="cover" />
               <div className="overlay">
