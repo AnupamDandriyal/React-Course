34bea91c67b98df1dba8aed987996e6adada71b6
AnupamDandriyal <112267752+AnupamDandriyal@users.noreply.github.com>
2024-11-16 20:18:05 +0530

    completed the feed and videoCard component

--- Youtube/src/components/Feed.jsx
+++ Youtube/src/components/Feed.jsx
@@ -16,8 +16,8 @@ const Feed = () => {
       <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-5">
           {/* Only render after searchResults is populated */}
-          {!loading &&
-            searchResults.map((item, i) => {
+          {!loading && searchResults &&
+            searchResults?.map((item, i) => {
               if (item.type !== "video") return null;
               return <VideoCard key={i} video={item?.video} />;
             })
--- Youtube/src/components/VideoCard.jsx
+++ Youtube/src/components/VideoCard.jsx
@@ -9,7 +9,32 @@ const VideoCard = ({video}) => {
       <div className="flex flex-col mb-8">
         <div className="relative mr-3 h-48 md:h-40 md:rounded-xl overflow-hidden">
           <img className='h-full w-full object-cover' src={video?.thumbnails[0]?.url} alt="video" />
-          {video.lengthSeconds && (<VideoLenghth />)}
+          {video?.lengthSeconds && (<VideoLenghth time={video?.lengthSeconds} />)}
+        </div>
+        <div className="flex text-white mt-3">
+          <div className="flex items-start">
+            <div className="flex h-9 w-9 rounded-full overflow-hidden">
+              <img src={video?.author?.avatar[0]?.url} alt="channel" className='w-full h-full object-cover' />
+            </div>
+          </div>
+          <div className='flex flex-col overflow-hidden mt-3'>
+            <span className='font-bold text-sm line-clamp-2'>
+              {video?.title}
+            </span>
+            <span className='text-[12px] font-semibold text-white/[0.7] mt-2 flex items-center'>
+              {video?.author?.title}
+              {video?.author?.badges[0]?.type === 'VERIFIED_CHANNEL' && (
+                <BsFillCheckCircleFill className='text-white/[0.5] text-[12px] ml-1'/>
+              )}
+            </span>
+            <div className="flex text-[12px] font-semibold text-white/[0.7] truncate overflow-hidden ">
+              <span>{`${abbreviateNumber(video?.stats?.views, 2)} views`} </span>
+              <span className="flex text-[24px] leading-none font-bold text-white/[0.7] top-[-10px] mx-1">.</span>
+              <span className='truncate'>
+                {video?.publishedTimeText}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </Link>
